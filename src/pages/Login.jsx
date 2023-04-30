4d65d464902f54f8703e60be85e147b01514f54f
esadakman <esadd26@gmail.com>
2023-04-30 23:04:03 +0300

    broken img links fixed

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -27,7 +27,7 @@ const Login = () => {
         lg={8}
         // lg={7}
         sx={{
-          backgroundImage: "url(https://source.unsplash.com/random)",
+          backgroundImage: "url(https://source.unsplash.com/user/erondu/1600x900)",
           backgroundRepeat: "no-repeat",
           backgroundColor: (t) =>
             t.palette.mode === "light"
